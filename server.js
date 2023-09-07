f486c02f24ffb4cc81e227a9922762ce4ac41bf3
treasure-praise <treasurepraiseolatunbosun@yahoo.com>
2023-09-07 12:06:49 +0100

    fixed utc timezone error

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 
 const app = express();
 
-const date = new Date();
 const daysOfWeek = [
   "Sunday",
   "Monday",
@@ -17,10 +16,14 @@ const PORT = process.env.PORT || 8000;
 
 app.get("/api", (req, res) => {
   const { slack_name, track } = req.query;
+  const date = new Date();
+  const utcTimeWithValidation = new Date(
+    date.getTime() + (date.getTimezoneOffset() + 120) * 60000
+  );
   res.status(200).json({
     slack_name: slack_name,
     current_day: daysOfWeek[date.getDay()],
-    utc_time: date,
+    utc_time: utcTimeWithValidation.toISOString(),
     track: track,
     github_file_url:
       "https://github.com/treasure-praise/HNG-Stage-1/blob/78f40da71a64db9f2c5d5c69d6051b45712dab8b/server.js",
