3748d5c60c99df9a9500d62c50a2d1530cc4ad8c
treasure-praise <treasurepraiseolatunbosun@yahoo.com>
2023-09-07 11:48:47 +0100

    fulfiled dynamic requirements

--- server.js
+++ server.js
@@ -15,12 +15,13 @@ const daysOfWeek = [
 
 const PORT = 8000;
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
+  const { slack_name, track } = req.query;
   res.status(200).json({
-    slack_name: "example_name",
+    slack_name: slack_name,
     current_day: daysOfWeek[date.getDay()],
     utc_time: date,
-    track: "backend",
+    track: track,
     github_file_url:
       "https://github.com/treasure-praise/HNG-Stage-1/blob/78f40da71a64db9f2c5d5c69d6051b45712dab8b/server.js",
     github_repo_url: "https://github.com/treasure-praise/HNG-Stage-1",
