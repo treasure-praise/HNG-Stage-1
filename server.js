ead111b7b8170ea5c34a78f02e2765c4bc5126df
treasure-praise <treasurepraiseolatunbosun@yahoo.com>
2023-09-07 11:41:52 +0100

    fulfiled all static requirements

--- server.js
+++ server.js
@@ -2,16 +2,28 @@ const express = require("express");
 
 const app = express();
 
+const date = new Date();
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
 const PORT = 8000;
 
 app.get("/", (req, res) => {
   res.status(200).json({
     slack_name: "example_name",
-    current_day: "Monday",
-    utc_time: "2023-08-21T15:04:05Z",
+    current_day: daysOfWeek[date.getDay()],
+    utc_time: date,
     track: "backend",
-    github_file_url: "https://github.com/username/repo/blob/main/file_name.ext",
-    github_repo_url: "https://github.com/username/repo",
+    github_file_url:
+      "https://github.com/treasure-praise/HNG-Stage-1/blob/78f40da71a64db9f2c5d5c69d6051b45712dab8b/server.js",
+    github_repo_url: "https://github.com/treasure-praise/HNG-Stage-1",
     status_code: 200,
   });
 });
