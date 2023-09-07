15f7dd57db4d4d2dad352423296d880961f41ff5
treasure-praise <treasurepraiseolatunbosun@yahoo.com>
2023-09-07 12:31:54 +0100

    updated utcTime

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const getCurrentUTC = require("./utils.js");
 
 const app = express();
 
@@ -17,13 +18,12 @@ const PORT = process.env.PORT || 8000;
 app.get("/api", (req, res) => {
   const { slack_name, track } = req.query;
   const date = new Date();
-  const utcTimeWithValidation = new Date(
-    date.getTime() + (date.getTimezoneOffset() + 120) * 60000
-  );
+  const utcTime = getCurrentUTC();
+
   res.status(200).json({
     slack_name: slack_name,
     current_day: daysOfWeek[date.getDay()],
-    utc_time: utcTimeWithValidation.toISOString(),
+    utc_time: utcTime,
     track: track,
     github_file_url:
       "https://github.com/treasure-praise/HNG-Stage-1/blob/78f40da71a64db9f2c5d5c69d6051b45712dab8b/server.js",
